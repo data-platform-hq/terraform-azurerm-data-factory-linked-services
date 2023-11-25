196bb16cfd3da066471c2810133dff249c02e5b4
owlleg6 <owlleg6@gmail.com>
2023-11-25 18:38:53 +0200

    fix: added key vault linked services

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+locals {
+  key_vault_parameters_mapped = { for object in var.key_vault_linked_services : object.name => object if object.name != null }
+}
+
+resource "azurerm_key_vault_access_policy" "this" {
+  for_each = local.key_vault_parameters_mapped
+
+  key_vault_id       = each.value.key_vault_id
+  tenant_id          = each.value.data_factory_tenant_id
+  object_id          = each.value.data_factory_object_id
+  secret_permissions = each.value.key_vault_policy_secret_permissions
+}
+
+resource "azurerm_data_factory_linked_service_key_vault" "this" {
+  for_each = local.key_vault_parameters_mapped
+
+  name            = each.value.name
+  key_vault_id    = each.value.key_vault_id
+  data_factory_id = var.data_factory_id
+}
+
 resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "this" {
   for_each = var.adls
 
--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "mssql" {
   value       = { for ls in azurerm_data_factory_linked_service_azure_sql_database.this : ls.name => ls.id }
   description = "Azure SQL Linked Service Name to Id map"
 }
+
+output "key_vault" {
+  value       = { for list in azurerm_data_factory_linked_service_key_vault.this : list.name => list }
+  description = "Azure Key Vault Linked Service Name to parameters map"
+}
